42caaec4bb48d1e0b55ef1a416e2a2d8d1d8cf9d
Nerijus <nerees@Nerijuss-Air.lan>
2022-04-13 12:55:24 +0300

    fix

--- django_pard/parduotuve/admin.py
+++ django_pard/parduotuve/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Order, Customer, Product, Product_order
+from .models import Order, Customer, Product, Product_order, Status
+
 
 admin.site.register(Order)
 admin.site.register(Customer)
 admin.site.register(Product_order)
 admin.site.register(Product)
+admin.site.register(Status)
--- django_pard/parduotuve/models.py
+++ django_pard/parduotuve/models.py
@@ -1,24 +1,46 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 class Product(models.Model):
+    name = models.CharField(max_length=200, help_text='Please make an order')
 
-    name = models.CharField('ID', max_length=200, help_text='Please make an order')
+    def __str__(self):
+        return self.name
+
+class Status(models.Model):
+    id = models.IntegerField(primary_key=1)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
- #
-from django.urls import reverse
+class Customer(models.Model):
+    """Model representing a Customer."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular customer instance."""
+        return reverse('customer-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name} {self.first_name}'
+
 
 class Order(models.Model):
-
     """Modelis reprezentuoja uzsakyma"""
-    ID = models.CharField('ID', max_length=200)
-    Customer_ID = models.ForeignKey('Customer_ID', on_delete=models.SET_NULL, null=True)
-    Date = models.TextField('date', max_length=1000, help_text='product ordered date')
-    Status_ID = models.CharField('Status_ID', max_length=13)
-    Product= models.ManyToManyField(Product, help_text='Choose the correct product for that order')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus užsakymo_ID')
+    Customer_ID = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    Date = models.DateTimeField(help_text='product ordered date')
+    Status_ID = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True)
+    Product = models.ManyToManyField(Product, help_text='Choose the correct product for that order')
 
     def __str__(self):
         return self.title
@@ -26,48 +48,15 @@ class Order(models.Model):
     def get_absolute_url(self):
         """Nurodo konkretaus uzsakymo atlikima"""
         return reverse('order-detail', args=[str(self.id)])
-import uuid
+
 
 class Product_order(models.Model):
     """Modelis, aprašantis produkto užsakymą"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus užsakymo_ID')
-    product_ID = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
-    quantity = models.DateField('yra', null=True, blank=True)
-
-    ORDER_STATUS = (
-        ('o', 'Ordering'),
-        ('b', 'Basket'),
-        ('c', 'completed'),
-        ('p', 'Paid'),
-    )
-
-    Status = models.CharField(
-        max_length=1,
-        choices=ORDER_STATUS,
-        blank=True,
-        default='o',
-        help_text='Status',
-    )
-
-    class Meta:
-        ordering = ['paid']
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus užsakymo_ID')
+    product_ID = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
 
-    class Customer(models.Model):
-        """Model representing a Customer."""
-        first_name = models.CharField('Name', max_length=100)
-        last_name = models.CharField('Lastname', max_length=100)
-
-        class Meta:
-            ordering = ['last_name', 'first_name']
-
-        def get_absolute_url(self):
-            """Returns the url to access a particular customer instance."""
-            return reverse('customer-detail', args=[str(self.id)])
-
-        def __str__(self):
-            """String for representing the Model object."""
-            return f'{self.last_name} {self.first_name}'
 
